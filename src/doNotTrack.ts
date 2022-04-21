68f434f0c366dd32ee0eb237e0d41b23b1e5dd38
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-04-21 00:01:31 -0400

    started working on noTracking function (NOT FINISHED)

---
+++ src/doNotTrack.ts
@@ -0,0 +1,14 @@
+let browserDoNotTrack: string = navigator.doNotTrack
+
+const getDoNotTrack = (browserDoNotTrack: string) => {
+    if (browserDoNotTrack == ) {
+
+    };
+    if (browserDoNotTrack == ) {
+
+    }
+}
+
+let doNotTrackStatus = getDoNotTrack(browserDoNotTrack);
+
+export { doNotTrackStatus, getDoNotTrack }
