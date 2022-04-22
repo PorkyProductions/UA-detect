53653add9c3ee831a0df1cea51f0367b2d897d5f
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-04-21 23:34:44 -0400

    style(prettier): :art: format index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,62 +5,78 @@ Licensed under the Apache-2.0 License.
 
 */
 
-
 // Browser
 
-import { browser, getBrowser } from './browser'
+import { browser, getBrowser } from "./browser";
 
 // Cookies
 
-import { getCookies, cookieStatus } from './cookies'
+import { getCookies, cookieStatus } from "./cookies";
 
 // Device Type
 
-import { DEVICE_type, DetectDeviceType } from './DetectDeviceType'
+import { DEVICE_type, DetectDeviceType } from "./DetectDeviceType";
 
 // Do Not Track
 
-import { doNotTrackStatus, getDoNotTrack } from './doNotTrack'
+import { doNotTrackStatus, getDoNotTrack } from "./doNotTrack";
 
 // Finite Mobile Device Type
 
-import { DEVICE_finiteType, finiteMobileDeviceType } from './finiteMobileDeviceType'
+import {
+  DEVICE_finiteType,
+  finiteMobileDeviceType,
+} from "./finiteMobileDeviceType";
 
 // Logical Processors
 
-import { browserSpecificSupportCores, processorCores } from './logicalProcessors'
+import {
+  browserSpecificSupportCores,
+  processorCores,
+} from "./logicalProcessors";
 
 // standard UA
 
-import { getCurrentUA, currentUA } from './getCurrentUA'
+import { getCurrentUA, currentUA } from "./getCurrentUA";
 
 //SCREEN ORIENTATION
 
-import { DetectScreenOrientation, ORIENTATION_isLandscape } from './orientation'
+import {
+  DetectScreenOrientation,
+  ORIENTATION_isLandscape,
+} from "./orientation";
 
 // RAM
 
-import { getRam, browserSpecificSupportRAM, browserRam } from './ram'
+import { getRam, browserSpecificSupportRAM, browserRam } from "./ram";
 
 // Exports
-
 
 // Make the functions accessible
 
 export {
-    DetectScreenOrientation,
-    DetectDeviceType,
-    finiteMobileDeviceType,
-    getCurrentUA,
-    getCookies,
-    getDoNotTrack,
-    getBrowser,
-    getRam,
-    browserSpecificSupportRAM,
-    browserSpecificSupportCores
+  DetectScreenOrientation,
+  DetectDeviceType,
+  finiteMobileDeviceType,
+  getCurrentUA,
+  getCookies,
+  getDoNotTrack,
+  getBrowser,
+  getRam,
+  browserSpecificSupportRAM,
+  browserSpecificSupportCores,
 };
-
 
 // As well as the returns on those functions
 
-export { ORIENTATION_isLandscape, DEVICE_type, DEVICE_finiteType, currentUA, cookieStatus, doNotTrackStatus, browser, browserRam, processorCores };
+export {
+  ORIENTATION_isLandscape,
+  DEVICE_type,
+  DEVICE_finiteType,
+  currentUA,
+  cookieStatus,
+  doNotTrackStatus,
+  browser,
+  browserRam,
+  processorCores,
+};
