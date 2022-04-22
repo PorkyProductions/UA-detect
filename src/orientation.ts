fa98288d8dd7b6043e763f1a576d6d78dfdd309d
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-04-21 23:41:52 -0400

    style(prettier): :art: format orientation.ts

--- src/orientation.ts
+++ src/orientation.ts
@@ -1,30 +1,25 @@
 /*
 
-Copyright © 2022, PorkyProductions, and it's contributors.
+Copyright ï¿½ 2022, PorkyProductions, and it's contributors.
 Licensed under the Apache-2.0 License.
 
 */
 
-
-
-
 //SCREEN ORIENTATION
 let ORIENTATION_isLandscape: boolean = true;
 $(document).ready((): void => {
-    DetectScreenOrientation();
-})
+  DetectScreenOrientation();
+});
 $(window).on("orientationchange", (_event): void => {
-    //When screen orientation changes
-    DetectScreenOrientation();
-
+  //When screen orientation changes
+  DetectScreenOrientation();
 });
 const DetectScreenOrientation = () => {
-    //detects orientation of device
-    ORIENTATION_isLandscape = (window.innerHeight <= window.innerWidth);
-    return ORIENTATION_isLandscape;
-}
-
+  //detects orientation of device
+  ORIENTATION_isLandscape = window.innerHeight <= window.innerWidth;
+  return ORIENTATION_isLandscape;
+};
 
 // Exports
 
-export { DetectScreenOrientation, ORIENTATION_isLandscape }
+export { DetectScreenOrientation, ORIENTATION_isLandscape };
