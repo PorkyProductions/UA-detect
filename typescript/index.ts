db32fc28fe6c1a24d2f8a28194ee656a3fc421d7
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-05-13 23:33:47 -0400

    fix: :fire: fixed the bug where the snippt was causing overflow

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,8 +1,20 @@
-import { DEVICE_type } from "uadetect";
+import { DEVICE_type, OS } from "uadetect";
 
 const downloadButtonHeader: HTMLElement = document.querySelector("download");
 
 if (DEVICE_type === "mobile") {
     downloadButtonHeader.style.display = "none";
+}
+
+if (DEVICE_type === "mobile") {
+    document.getElementById("snippet").style.display = "none";
+}
+
+if (OS === "iOS") {
+    document.getElementById("snippet").style.display = "none";
+}
+
+if (OS === "Android") {
+    document.getElementById("snippet").style.display = "none";
 }
 
