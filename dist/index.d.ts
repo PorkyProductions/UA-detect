8a775cee794848541d1ea73716e14112e83af1ee
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-05-20 09:15:25 -0400

    wrote more typings

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -10,44 +10,44 @@ Licensed under the Apache-2.0 License.
 declare module 'uadetect';
 
 // ORIENTATION
-declare type ORIENTATION_isLandscape = boolean;
+export type ORIENTATION_isLandscape = boolean;
 
 // DEVICE TYPES
 
-declare type DEVICE_type = string;
-declare type DEVICE_finiteType = string;
+export type DEVICE_type = string;
+export type DEVICE_finiteType = string;
 
 // COOKIES
 
-declare type cookieStatus = boolean;
+export type cookieStatus = boolean;
 
 // Do Not Track
 
-declare type doNotTrackStatus = boolean;
+export type doNotTrackStatus = boolean;
 
 // Browser
-declare type browser = string;
+export type browser = string;
 
 // Browser Ram
-declare type browserRam = number;
+export type browserRam = number;
 
 // Build Number
-declare type buildNumber = number;
+export type buildNumber = number;
 
 // Processor Cores
-declare type processorCores = number;
+export type processorCores = number;
 
 // Max Touch Points
-declare type maxTouchPoints = number;
+export type maxTouchPoints = number;
 
 // Online Status
-declare type browserOnlineStatus = boolean;
+export type browserOnlineStatus = boolean;
 
 // PDF Support
-declare type PDFviewerStatus = boolean;
+export type PDFviewerStatus = boolean;
 
 // Robot Status
-declare type robotStatus = boolean;
+export type robotStatus = boolean;
 
 // OS
-declare type OS = string;
+export type OS = string;
