b62d18f007010b5e5d9988621ba4bd1f99c6623b
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-05-21 09:33:51 -0400

    took out the jQuery

--- src/orientation.ts
+++ src/orientation.ts
@@ -7,18 +7,15 @@ Licensed under the Apache-2.0 License.
 
 //SCREEN ORIENTATION
 let ORIENTATION_isLandscape: boolean = true;
-$(document).ready((): void => {
-  DetectScreenOrientation();
-});
-$(window).on("orientationchange", (_event): void => {
-  //When screen orientation changes
-  DetectScreenOrientation();
-});
-const DetectScreenOrientation = () => {
+
+const DetectScreenOrientation = (): boolean => {
   //detects orientation of device
   ORIENTATION_isLandscape = window.innerHeight <= window.innerWidth;
   return ORIENTATION_isLandscape;
 };
+
+window.addEventListener("resize", DetectScreenOrientation);
+
 
 // Exports
 
