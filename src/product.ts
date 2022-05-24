7001019bca13b354c314da1627c427ddb50c5aa2
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-05-23 22:07:33 -0400

    feat: :sparkles: add the product component

---
+++ src/product.ts
@@ -0,0 +1,22 @@
+let productID = window.navigator.product;
+
+
+const getProductID = (productID): String | 'Gecko' | 'WebKit' | 'Trident' | 'Presto' | 'Other' | Error => {
+    if (productID === 'Gecko') {
+        return 'Gecko'
+    }
+    else if (productID === 'WebKit') {
+        return 'WebKit'
+    }
+    else if (productID === 'Trident') {
+        return 'Trident'
+    }
+    else if (productID === 'Presto') {
+        return 'Presto'
+    }
+    else {
+        throw new Error("Unknown engine \(UAD_1702\)")  // throw error;
+    }
+}
+
+let product = getProductID(productID);
