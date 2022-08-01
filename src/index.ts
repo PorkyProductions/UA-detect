b18622903d1a331725785e0be67a3519f6aae546
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-08-01 11:35:55 -0400

    fix(types): :label: update "String" to match the real "string"

--- src/index.ts
+++ src/index.ts
@@ -145,7 +145,7 @@ export const UADetect = {
     return DetectScreenOrientation()
   },
   detectFiniteMobileDeviceType() {
-    return finiteMobileDeviceType()
+    return finiteMobileDeviceType() 
   }
-  
+
 }
