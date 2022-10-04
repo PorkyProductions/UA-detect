660b4335e99a3fc8f4022144801f9b0536561e9c
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-04 15:37:43 -0400

    fix: :bug: fixes for UAD 4.1

--- dist/geolocation.js
+++
@@ -1,7 +0,0 @@
-export let lat;
-export let lon;
-navigator.geolocation.getCurrentPosition((position) => {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    return lat && lon;
-}, () => console.error(`Cannot get current position. This is either because the browser doesn't support it, or the user has denied access `));
