0e28bfc93ebd65e4884f4f1b890abd7c412448c2
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 21:34:16 -0400

    refactor(lint): :rotating_light: add eslint and fix `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -8,48 +8,48 @@ Licensed under the Apache-2.0 License.
 
 // Browser
 
-import { browser, getBrowser } from "./browser";
+import { browser, getBrowser } from './browser';
 
 // Code Name
 
-import { codeName, getCodeName } from "./appCodeName";
+import { codeName, getCodeName } from './appCodeName';
 
 // Cookies
 
-import { getCookies, cookieStatus } from "./cookies";
+import { getCookies, cookieStatus } from './cookies';
 
 // Device Type
 
-import { DEVICE_type, DetectDeviceType } from "./DetectDeviceType";
+import { DEVICE_type, DetectDeviceType } from './DetectDeviceType';
 
 // Do Not Track
 
-import { doNotTrackStatus, getDoNotTrack } from "./doNotTrack";
+import { doNotTrackStatus, getDoNotTrack } from './doNotTrack';
 
 // Finite Mobile Device Type
 
 import {
-  DEVICE_finiteType,
-  finiteMobileDeviceType,
-} from "./finiteMobileDeviceType";
+	DEVICE_finiteType,
+	finiteMobileDeviceType,
+} from './finiteMobileDeviceType';
 
 // Geolocation
 
 import {
-  lat,
-  lon
-} from "./geolocation";
+	lat,
+	lon
+} from './geolocation';
 
 // Logical Processors
 
 import {
-  browserSpecificSupportCores,
-  processorCores,
-} from "./logicalProcessors";
+	browserSpecificSupportCores,
+	processorCores,
+} from './logicalProcessors';
 
 // Max Touch Points
 
-import { maxTouchPoints, getMaxTouchPoints } from "./maxTouchPoints";
+import { maxTouchPoints, getMaxTouchPoints } from './maxTouchPoints';
 
 // Navigator Object
 
@@ -57,26 +57,26 @@ import { navigatorObject, getterForNavigator } from './navigator';
 
 // Online Status
 
-import { browserOnlineStatus, getBrowserIsOnline } from "./online";
+import { browserOnlineStatus, getBrowserIsOnline } from './online';
 
 // standard UA
 
-import { getCurrentUA, currentUA } from "./getCurrentUA";
+import { getCurrentUA, currentUA } from './getCurrentUA';
 
 // Operating System
 
-import { OS, getOS } from "./operatingSystem";
+import { OS, getOS } from './operatingSystem';
 
 //SCREEN ORIENTATION
 
 import {
-  DetectScreenOrientation,
-  ORIENTATION_isLandscape,
-} from "./orientation";
+	DetectScreenOrientation,
+	ORIENTATION_isLandscape,
+} from './orientation';
 
 // PDF Viewer
 
-import { PDFviewerStatus, getPDF } from "./pdf";
+import { PDFviewerStatus, getPDF } from './pdf';
 
 // Product
 
@@ -84,92 +84,92 @@ import {engine, getProductID} from './product';
 
 // Version
 
-import {version, getAppVersion} from './appVersion'
+import {version, getAppVersion} from './appVersion';
 
 // Webdriver
 
-import { robotStatus, getBots } from "./webdriver";
+import { robotStatus, getBots } from './webdriver';
 
 // Exports
 
-// Make the functions accessible
+// Make the unknowns accessible
 
 export {
-  DetectScreenOrientation,
-  DetectDeviceType,
-  finiteMobileDeviceType,
-  getCurrentUA,
-  getCookies,
-  getDoNotTrack,
-  getBrowser,
-  browserSpecificSupportCores,
-  getMaxTouchPoints,
-  getOS,
-  getCodeName,
-  getterForNavigator,
-  getBrowserIsOnline,
-  getPDF,
-  getAppVersion,
-  getBots,
-  getProductID,
+	DetectScreenOrientation,
+	DetectDeviceType,
+	finiteMobileDeviceType,
+	getCurrentUA,
+	getCookies,
+	getDoNotTrack,
+	getBrowser,
+	browserSpecificSupportCores,
+	getMaxTouchPoints,
+	getOS,
+	getCodeName,
+	getterForNavigator,
+	getBrowserIsOnline,
+	getPDF,
+	getAppVersion,
+	getBots,
+	getProductID,
 };
 
-// As well as the returns on those functions
+// As well as the returns on those unknowns
 
 export {
-  ORIENTATION_isLandscape as orientationIsLandscape,
-  DEVICE_type as deviceType,
-  DEVICE_finiteType as deviceFiniteType,
-  currentUA,
-  cookieStatus,
-  doNotTrackStatus,
-  browser,
-  processorCores,
-  maxTouchPoints,
-  navigatorObject,
-  browserOnlineStatus,
-  PDFviewerStatus,
-  robotStatus,
-  OS,
-  lat,
-  lon,
-  codeName,
-  engine,
-  version
+	ORIENTATION_isLandscape as orientationIsLandscape,
+	DEVICE_type as deviceType,
+	DEVICE_finiteType as deviceFiniteType,
+	currentUA,
+	cookieStatus,
+	doNotTrackStatus,
+	browser,
+	processorCores,
+	maxTouchPoints,
+	navigatorObject,
+	browserOnlineStatus,
+	PDFviewerStatus,
+	robotStatus,
+	OS,
+	lat,
+	lon,
+	codeName,
+	engine,
+	version
 };
 
 export interface UADetect {
-  getDeviceType: Function,
-  getScreenOrientation: Function,
-  getFiniteMobileDeviceType: Function,
-  getCurrentUA: Function,
-  getCookieStatus: Function,
-  getDoNotTrackStatus: Function
-  getBrowser: Function,
-  getProcessorCores: Function,
-  getMaxTouchPoints: Function,
-  getNavigatorObject: Function,
-  getBrowserOnlineStatus: Function,
-  getPDFviewerStatus: Function,
-  getRobotStatus: Function,
-  getOS: Function,
-  getCodeName: Function,
-  getEngine: Function,
-  getVersion: Function,
+  getDeviceType: unknown,
+  getScreenOrientation: unknown,
+  getFiniteMobileDeviceType: unknown,
+  getCurrentUA: unknown,
+  getCookieStatus: unknown,
+  getDoNotTrackStatus: unknown
+  getBrowser: unknown,
+  getProcessorCores: unknown,
+  getMaxTouchPoints: unknown,
+  getNavigatorObject: unknown,
+  getBrowserOnlineStatus: unknown,
+  getPDFviewerStatus: unknown,
+  getRobotStatus: unknown,
+  getOS: unknown,
+  getCodeName: unknown,
+  getEngine: unknown,
+  getVersion: unknown,
   orientationIsLandscape: boolean,
-  deviceType: "tablet" | "mobile" | "desktop",
-  deviceFiniteType: "Android" | "iOS" | "Unknown" | Error | "BlackBerry" | "Windows Phone" | "webOS",
+  deviceType: 'tablet' | 'mobile' | 'desktop',
+  deviceFiniteType: 'Android' | 'iOS' | 'Unknown' | Error | 'BlackBerry' | 'Windows Phone' | 'webOS',
   currentUA: string,
-  cookieStatus: "cookiesEnabled" | "cookiesNotEnabled" | "ERROR",
-  doNotTrackStatus: "trackingAllowed" | "trackingNotAllowed" | "trackingUnspecified" | "ERROR"
-  browser: "Opera" | "Chrome" | "Firefox" | "Safari" | "IE" | "Edge" | "unknown" | undefined,
+  cookieStatus: 'cookiesEnabled' | 'cookiesNotEnabled' | 'ERROR',
+  doNotTrackStatus: 'trackingAllowed' | 'trackingNotAllowed' | 'trackingUnspecified' | 'ERROR'
+  browser: 'Opera' | 'Chrome' | 'Firefox' | 'Safari' | 'IE' | 'Edge' | 'unknown' | undefined,
   processorCores: number | undefined,
   maxTouchPoints: number,
   navigatorObject: object,
-  browserOnlineStatus: "browserOnline" | "browserOffline",
-  PDFviewerStatus: "PDFviewerEnabled" | "PDFviewerDisabled",
-  robotStatus: "robotControlled" | "humanControlled" | "ERROR",
-  OS: "Windows" | "Mac" | "Linux" | "Android" | "iOS" | "Unknown",
+  browserOnlineStatus: 'browserOnline' | 'browserOffline',
+  PDFviewerStatus: 'PDFviewerEnabled' | 'PDFviewerDisabled',
+  robotStatus: 'robotControlled' | 'humanControlled' | 'ERROR',
+  OS: 'Windows' | 'Mac' | 'Linux' | 'Android' | 'iOS' | 'Unknown',
   lat: number,
   lon: number,
   codeName: string | Error,
@@ -180,76 +180,76 @@ export interface UADetect {
 // Create the UADetect Object
 
 export const UADetect: UADetect = {
-  getDeviceType() {
-    return DetectDeviceType()
-  },
-  getScreenOrientation() {
-    return DetectScreenOrientation()
-  },
-  getFiniteMobileDeviceType() {
-    return finiteMobileDeviceType() 
-  },
-  getCurrentUA() {
-    return getCurrentUA()
-  },
-  getCookieStatus(cookies: boolean) {
-    return getCookies(cookies)
-  },
-  getDoNotTrackStatus(browserDoNotTrack: string | number | null) {
-    return getDoNotTrack(browserDoNotTrack)
-  },
-  getBrowser({ ua }: { ua: string; }) {
-    return getBrowser({ ua })
-  },
-  getProcessorCores() {
-    return browserSpecificSupportCores()
-  },
-  getMaxTouchPoints() {
-    return getMaxTouchPoints()
-  },
-  getNavigatorObject(navigatorObjectDev: Navigator) {
-    return getterForNavigator(navigatorObjectDev)
-  },
-  getBrowserOnlineStatus({ browserIsOnline }: { browserIsOnline: boolean; }) {
-    return getBrowserIsOnline({ browserIsOnline });
-  },
-  getPDFviewerStatus({ pdf }: { pdf: boolean; }) {
-    return getPDF({ pdf });
-  },
-  getRobotStatus({ webdriverControlled }: { webdriverControlled: boolean; }) {
-    return getBots({ webdriverControlled })
-  },
-  getOS({ ua }: { ua: string; }) {
-    return getOS({ ua: navigator.userAgent })
-  },
-  getCodeName(appCodeName: string) {
-    return getCodeName(appCodeName)
-  },
-  getEngine(productID: string) {
-    return getProductID(productID)
-  },
-  getVersion(appVersion: string) {
-    return getAppVersion(appVersion)
-  },
-  // From here, we can then begin to call the returns on those functions here
-  // Most of them are just transferring the name over
-  orientationIsLandscape: ORIENTATION_isLandscape,
-  deviceType: DEVICE_type,
-  deviceFiniteType: DEVICE_finiteType,
-  currentUA: currentUA,
-  cookieStatus: cookieStatus,
-  doNotTrackStatus: doNotTrackStatus,
-  browser: browser,
-  processorCores: processorCores,
-  maxTouchPoints: maxTouchPoints,
-  navigatorObject: navigatorObject,
-  browserOnlineStatus: browserOnlineStatus,
-  PDFviewerStatus: PDFviewerStatus,
-  robotStatus: robotStatus,
-  OS: OS,
-  lat: lat,
-  lon: lon,
-  codeName: codeName,
-  engine: engine,
-  version: version
-}
+	getDeviceType() {
+		return DetectDeviceType();
+	},
+	getScreenOrientation() {
+		return DetectScreenOrientation();
+	},
+	getFiniteMobileDeviceType() {
+		return finiteMobileDeviceType(); 
+	},
+	getCurrentUA() {
+		return getCurrentUA();
+	},
+	getCookieStatus(cookies: boolean) {
+		return getCookies(cookies);
+	},
+	getDoNotTrackStatus(browserDoNotTrack: string | number | null) {
+		return getDoNotTrack(browserDoNotTrack);
+	},
+	getBrowser({ ua }: { ua: string; }) {
+		return getBrowser({ ua });
+	},
+	getProcessorCores() {
+		return browserSpecificSupportCores();
+	},
+	getMaxTouchPoints() {
+		return getMaxTouchPoints();
+	},
+	getNavigatorObject(navigatorObjectDev: Navigator) {
+		return getterForNavigator(navigatorObjectDev);
+	},
+	getBrowserOnlineStatus({ browserIsOnline }: { browserIsOnline: boolean; }) {
+		return getBrowserIsOnline({ browserIsOnline });
+	},
+	getPDFviewerStatus({ pdf }: { pdf: boolean; }) {
+		return getPDF({ pdf });
+	},
+	getRobotStatus({ webdriverControlled }: { webdriverControlled: boolean; }) {
+		return getBots({ webdriverControlled });
+	},
+	getOS() {
+		return getOS({ ua: navigator.userAgent });
+	},
+	getCodeName(appCodeName: string) {
+		return getCodeName(appCodeName);
+	},
+	getEngine(productID: string) {
+		return getProductID(productID);
+	},
+	getVersion(appVersion: string) {
+		return getAppVersion(appVersion);
+	},
+	// From here, we can then begin to call the returns on those unknowns here
+	// Most of them are just transferring the name over
+	orientationIsLandscape: ORIENTATION_isLandscape,
+	deviceType: DEVICE_type,
+	deviceFiniteType: DEVICE_finiteType,
+	currentUA: currentUA,
+	cookieStatus: cookieStatus,
+	doNotTrackStatus: doNotTrackStatus,
+	browser: browser,
+	processorCores: processorCores,
+	maxTouchPoints: maxTouchPoints,
+	navigatorObject: navigatorObject,
+	browserOnlineStatus: browserOnlineStatus,
+	PDFviewerStatus: PDFviewerStatus,
+	robotStatus: robotStatus,
+	OS: OS,
+	lat: lat,
+	lon: lon,
+	codeName: codeName,
+	engine: engine,
+	version: version
+};
