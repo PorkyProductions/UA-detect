fe07e298cc8bf184527866be625a34cb25aeb098
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 21:39:03 -0400

    refactor(lint): :rotating_light: fix browser lint issues

--- src/cookies.ts
+++ src/cookies.ts
@@ -1,19 +1,19 @@
-let cookies: boolean = navigator.cookieEnabled
+const cookies: boolean = navigator.cookieEnabled;
 
-const getCookies = (cookies: boolean): "cookiesEnabled" | "cookiesNotEnabled" | "ERROR" => {
-    if (cookies === true) {
-        return "cookiesEnabled"
-    };
-    if (cookies === false) {
-        return "cookiesNotEnabled"
-    }
-    else {
-        console.error("ERR: Cookie status can not be determined \(UAD_201\)");
-        return "ERROR"
-    }
-}
+const getCookies = (cookies: boolean): 'cookiesEnabled' | 'cookiesNotEnabled' | 'ERROR' => {
+	if (cookies === true) {
+		return 'cookiesEnabled';
+	}
+	if (cookies === false) {
+		return 'cookiesNotEnabled';
+	}
+	else {
+		console.error('ERR: Cookie status can not be determined (UAD_201)');
+		return 'ERROR';
+	}
+};
 
-let cookieStatus = getCookies(cookies);
+const cookieStatus = getCookies(cookies);
 
-export { cookieStatus }
-export { getCookies }
+export { cookieStatus };
+export { getCookies };
