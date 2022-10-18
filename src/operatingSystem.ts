80f06253fa13c59d509fd70a5ef5ff18c651e6be
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 22:02:36 -0400

    refactor(lint): :rotating_light: fix OS lint issues

--- src/operatingSystem.ts
+++ src/operatingSystem.ts
@@ -1,32 +1,32 @@
-const getOS = ({ ua }: { ua: string; }): "Windows" | "Mac" | "Linux" | "Android" | "iOS" | "Unknown" => {
-    if (ua.indexOf('Windows') > -1) {
-        return 'Windows';
-    }
-    else if (ua.indexOf('Mac') > -1) {
-        return 'Mac';
-    }
-    else if (ua.indexOf('Linux') > -1) {
-        return 'Linux';
-    }
-    else if (ua.indexOf('Android') > -1) {
-        return 'Android';
-    }
-    else if (ua.indexOf('iPhone') > -1) {
-        return 'iOS';
-    }
-    else if (ua.indexOf('iPad') > -1) {
-        return 'iOS';
-    }
-    else if (ua.indexOf('iPod') > -1) {
-        return 'iOS';
-    }
-    else {
-        console.error("Unknown Operating System\(UAD_1101\)");
-        return "Unknown";
+const getOS = ({ ua }: { ua: string; }): 'Windows' | 'Mac' | 'Linux' | 'Android' | 'iOS' | 'Unknown' => {
+	if (ua.indexOf('Windows') > -1) {
+		return 'Windows';
+	}
+	else if (ua.indexOf('Mac') > -1) {
+		return 'Mac';
+	}
+	else if (ua.indexOf('Linux') > -1) {
+		return 'Linux';
+	}
+	else if (ua.indexOf('Android') > -1) {
+		return 'Android';
+	}
+	else if (ua.indexOf('iPhone') > -1) {
+		return 'iOS';
+	}
+	else if (ua.indexOf('iPad') > -1) {
+		return 'iOS';
+	}
+	else if (ua.indexOf('iPod') > -1) {
+		return 'iOS';
+	}
+	else {
+		console.error('Unknown Operating System (UAD_1101)');
+		return 'Unknown';
         
-    }
-}
+	}
+};
 
-let OS = getOS({ ua: navigator.userAgent });
+const OS = getOS({ ua: navigator.userAgent });
 
-export { OS, getOS }
+export { OS, getOS };
