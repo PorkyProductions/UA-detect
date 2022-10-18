f696daa1d798830b68bc8c3ba693eecb570b2d89
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 21:42:04 -0400

    refactor(lint): :rotating_light: fix geolocation lint issues

--- src/geolocation.ts
+++ src/geolocation.ts
@@ -2,25 +2,25 @@ export let lat: number;
 export let lon: number;
 
 const latitudeGetter = (): number => {
-    navigator.geolocation.getCurrentPosition((position): number => {
-        lat = position.coords.latitude;
-        return lat;
-    },
-        () => console.error(`Cannot get current latitude. This is either because the browser doesn't support it, or the user has denied access `)
-    );
-    return lat;
+	navigator.geolocation.getCurrentPosition((position): number => {
+		lat = position.coords.latitude;
+		return lat;
+	},
+	() => console.error('Cannot get current latitude. This is either because the browser doesn\'t support it, or the user has denied access ')
+	);
+	return lat;
 };
 
 const longitiudeGetter = (): number => {
-    navigator.geolocation.getCurrentPosition((position) => {
-        lon = position.coords.longitude;
-        return lon
-    },
-        () => console.error(`Cannot get current longitude. This is either because the browser doesn't support it, or the user has denied access`),
-        undefined
-    )
-    return lon
-}
+	navigator.geolocation.getCurrentPosition((position) => {
+		lon = position.coords.longitude;
+		return lon;
+	},
+	() => console.error('Cannot get current longitude. This is either because the browser doesn\'t support it, or the user has denied access'),
+	undefined
+	);
+	return lon;
+};
 
 lat = latitudeGetter();
 lon = longitiudeGetter();
