564dd83a137261308028caa7a983608e4c897afc
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 21:41:40 -0400

    refactor(lint): :rotating_light: fix finiteMobileDeviceType lint issues

--- src/finiteMobileDeviceType.ts
+++ src/finiteMobileDeviceType.ts
@@ -1,31 +1,31 @@
 ﻿// Finite Device Type
-export default function finiteMobileDeviceType(): "iOS" | "Android" | "BlackBerry" | "Windows Phone" | "webOS" | "Unknown" | Error {
-    const ua = navigator.userAgent;
-    if (/(iPhone|iPod|iPad)/i.test(ua)) {
-        console.log("iOS");
-        return "iOS";
-    }
-    else if (/Android/i.test(ua)) {
-        console.log("Android");
-        return "Android";
-    }
-    else if (/BlackBerry/i.test(ua)) {
-        console.log("BlackBerry");
-        return "BlackBerry";
-    }
-    else if (/Windows Phone/i.test(ua)) {
-        console.log("Windows Phone");
-        return "Windows Phone";
-    }
-    else if (/webOS/i.test(ua)) {
-        console.log("webOS");
-        return "webOS";
-    }
-    else {
-        console.error("Unknown device type\(UAD_601\)");
-        return "Unknown";
-    }
+export default function finiteMobileDeviceType(): 'iOS' | 'Android' | 'BlackBerry' | 'Windows Phone' | 'webOS' | 'Unknown' | Error {
+	const ua = navigator.userAgent;
+	if (/(iPhone|iPod|iPad)/i.test(ua)) {
+		console.log('iOS');
+		return 'iOS';
+	}
+	else if (/Android/i.test(ua)) {
+		console.log('Android');
+		return 'Android';
+	}
+	else if (/BlackBerry/i.test(ua)) {
+		console.log('BlackBerry');
+		return 'BlackBerry';
+	}
+	else if (/Windows Phone/i.test(ua)) {
+		console.log('Windows Phone');
+		return 'Windows Phone';
+	}
+	else if (/webOS/i.test(ua)) {
+		console.log('webOS');
+		return 'webOS';
+	}
+	else {
+		console.error('Unknown device type (UAD_601)');
+		return 'Unknown';
+	}
 }
-let finiteDeviceType = finiteMobileDeviceType();
+const finiteDeviceType = finiteMobileDeviceType();
 
-export { finiteDeviceType as DEVICE_finiteType, finiteMobileDeviceType }
+export { finiteDeviceType as DEVICE_finiteType, finiteMobileDeviceType };
