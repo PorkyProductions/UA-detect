33d658af26c4c36bcf27d74669af03fb280edeac
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 22:03:06 -0400

    refactor(lint): :rotating_light: fix browser lint issues

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,16 +1,16 @@
-let pdf: boolean = navigator.pdfViewerEnabled;
+const pdf: boolean = navigator.pdfViewerEnabled;
 
 
-export const getPDF = ({ pdf }: { pdf: boolean; }): "PDFviewerEnabled" | "PDFviewerDisabled" => {
-    if (pdf) {
-        return "PDFviewerEnabled";
-    }
-    else {
-        return "PDFviewerDisabled";
-    }
-}
+export const getPDF = ({ pdf }: { pdf: boolean; }): 'PDFviewerEnabled' | 'PDFviewerDisabled' => {
+	if (pdf) {
+		return 'PDFviewerEnabled';
+	}
+	else {
+		return 'PDFviewerDisabled';
+	}
+};
 
-let PDFviewerStatus = getPDF({ pdf });
+const PDFviewerStatus = getPDF({ pdf });
 
 export { PDFviewerStatus };
 
