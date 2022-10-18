0f3bb318e737f21cc5b3d1f2a043448ec00e6856
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 22:03:50 -0400

    refactor(lint): :rotating_light: fix robot lint issues

--- src/webdriver.ts
+++ src/webdriver.ts
@@ -1,14 +1,14 @@
-let webdriverControlled: boolean = navigator.webdriver
+const webdriverControlled: boolean = navigator.webdriver;
 
-export const getBots = ({ webdriverControlled }: { webdriverControlled: boolean; }): "robotControlled" | "humanControlled" | "ERROR" => {
-    if (webdriverControlled) { 
-        return "robotControlled";
-    }
-    else {
-        return "humanControlled";
-    }
+export const getBots = ({ webdriverControlled }: { webdriverControlled: boolean; }): 'robotControlled' | 'humanControlled' | 'ERROR' => {
+	if (webdriverControlled) { 
+		return 'robotControlled';
+	}
+	else {
+		return 'humanControlled';
+	}
 };
 
-let robotStatus: "robotControlled" | "humanControlled" | "ERROR" = getBots({ webdriverControlled });
+const robotStatus: 'robotControlled' | 'humanControlled' | 'ERROR' = getBots({ webdriverControlled });
 
 export { robotStatus, webdriverControlled };
