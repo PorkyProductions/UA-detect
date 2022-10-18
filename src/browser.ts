875cf5f06a2c85fad18b87ccd3c79e08a4026e22
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 21:38:25 -0400

    refactor(lint): :rotating_light: fix browser lint issues

--- src/browser.ts
+++ src/browser.ts
@@ -1,35 +1,35 @@
-let ua = navigator.userAgent;
+const ua = navigator.userAgent;
 
 
-const getBrowser = ({ ua }: { ua: string; }): "Opera" | "Chrome" | "Firefox" | "Safari" | "IE" | "Edge" | "unknown" | undefined => {
-    if (ua.indexOf('Opera') > -1) {
-        return 'Opera';
-    }
-    else if (ua.indexOf('Chrome') > -1) {
-        return 'Chrome';
-    }
-    else if (ua.indexOf('Firefox') > -1) {
-        return 'Firefox';
-    }
-    else if (ua.indexOf('Safari') > -1) {
-        return 'Safari';
-    }
-    else if (ua.indexOf('MSIE') > -1) {
-        return 'IE';
-    }
-    else if (ua.indexOf('Trident') > -1) {
-        return 'IE';
-    }
-    else if (ua.indexOf('Edge') > -1) {
-        return 'Edge';
-    }
-    else {
-        console.error("ERR: Browser can not be determined \(UAD_101\)"); 
-        "Unknown";
-    }
-    return
+const getBrowser = ({ ua }: { ua: string; }): 'Opera' | 'Chrome' | 'Firefox' | 'Safari' | 'IE' | 'Edge' | 'unknown' | undefined => {
+	if (ua.indexOf('Opera') > -1) {
+		return 'Opera';
+	}
+	else if (ua.indexOf('Chrome') > -1) {
+		return 'Chrome';
+	}
+	else if (ua.indexOf('Firefox') > -1) {
+		return 'Firefox';
+	}
+	else if (ua.indexOf('Safari') > -1) {
+		return 'Safari';
+	}
+	else if (ua.indexOf('MSIE') > -1) {
+		return 'IE';
+	}
+	else if (ua.indexOf('Trident') > -1) {
+		return 'IE';
+	}
+	else if (ua.indexOf('Edge') > -1) {
+		return 'Edge';
+	}
+	else {
+		console.error('ERR: Browser can not be determined (UAD_101)'); 
+		'Unknown';
+	}
+	return;
 };
 
-let browser = getBrowser({ ua });
+const browser = getBrowser({ ua });
 
 export { browser, getBrowser };
