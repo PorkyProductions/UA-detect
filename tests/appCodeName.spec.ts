300d4ea2598ebc0d3ca3b70b211e1048c239a29f
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-17 22:05:04 -0400

    refactor(lint): :rotating_light: 🧪 fix lint issues on tests

--- tests/appCodeName.spec.ts
+++ tests/appCodeName.spec.ts
@@ -1,9 +1,9 @@
 describe('appCodeName is properly detected', () => {
-    const nav = window.navigator
-    let appCodeName = window.navigator.appCodeName
-    beforeEach(() => {
-        expect(nav).toBe(navigator)
-        expect(typeof(appCodeName)).toBe("string")
-    })
-    test.todo('makes sure the correct returns are created')
-})
+	const nav = window.navigator;
+	const appCodeName = window.navigator.appCodeName;
+	beforeEach(() => {
+		expect(nav).toBe(navigator);
+		expect(typeof(appCodeName)).toBe('string');
+	});
+	test.todo('makes sure the correct returns are created');
+});
