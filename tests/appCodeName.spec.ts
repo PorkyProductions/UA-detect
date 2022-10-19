a8207435e8a32d6b3da130ea0743e7626121e91b
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-10-19 09:54:28 -0400

    test(rm): :fire: remove broken test

--- tests/appCodeName.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('appCodeName is properly detected', () => {
-	const nav = window.navigator;
-	const appCodeName = window.navigator.appCodeName;
-	beforeEach(() => {
-		expect(nav).toBe(navigator);
-		expect(typeof(appCodeName)).toBe('string');
-	});
-	test.todo('makes sure the correct returns are created');
-});
