3b8a6a6a053aaef98bcdb9dbafc05b95a7c6f220
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2023-02-03 19:32:51 -0500

    chore: :page_facing_up: update license with new year

--- src/online.ts
+++ src/online.ts
@@ -1,6 +1,6 @@
 /**
 * @license
-* Copyright 2022, PorkyProductions, and contributors
+* Copyright 2023, PorkyProductions, and contributors
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
